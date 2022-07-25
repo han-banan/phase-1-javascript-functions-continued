8050ff6796f94a01f5c47bcf7c6e58d23fb3c9bf
Hannahbronwing <hannahsdoc.2015@gmail.com>
2022-07-25 17:18:20 -0400

    commit tree 1229596384da17e7c2001c992f8f7191dd28b031

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function saturdayFun(){
+    console.log ()
+} saturdayFun()
+function saturdayFun(activity = 'roller-skate'){
+    console.log(`This Saturday, I want to ${activity}!`)
+}
+saturdayFun('bathe my dog')
